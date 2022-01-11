65aed7aaa33d09654a60a2fa3abfb3f7a5a8d8cd
FiTo <claudiopero@estudiopero.com>
2022-01-11 02:26:46 +0000

    004 uso de todo los componentes y pagina bien

--- resources/js/components/PostsComponent.vue
+++ resources/js/components/PostsComponent.vue
@@ -2,13 +2,17 @@
 	<div class="container">
 		<div class="row justify-content-center">
 			<div class="col-md-8">
-				<div class="card card-default">
-					<div class="card-header">Posts Component</div>
+
+				<div class="card mb-3 mt-3" v-for="item in list">
+					<a class="card-header" v-bind:href="item.slug" v-text="item.title"></a>
 
 					<div class="card-body">
-						-------
+						<p>ID: {{item.id}}</p>
+						<p class="card-text" v-text="item.body"></p>
 					</div>
 				</div>
+
+				<infinite-loading @infinite="infiniteHandler"></infinite-loading>
 			</div>
 		</div>
 	</div>
@@ -16,8 +20,29 @@
 
 <script>
 	export default {
-		mounted(){
-			console.log('Component mounted.')
+		data(){
+			return {
+				list: [],
+				page: 0
+			};
+		},
+		methods: {
+			infiniteHandler($state){
+				this.page++
+				let url = 'http://localhost/scrollinfiniteVyL/public/api/posts?page=' + this.page
+			
+				axios.get(url)
+				.then(response => {
+					let posts = response.data.data
+
+					if(posts.length){
+						this.list = this.list.concat(posts)
+						$state.loaded()
+					}else{
+						$state.complete()
+					}
+				});
+			}
 		}
 	}
 </script>
